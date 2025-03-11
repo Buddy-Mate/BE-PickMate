1d4f6f595a4aa0e35788bb5634dbd5ddbfd3a876
pangminwhere <qoendqh@naver.com>
2025-03-12 00:08:48 +0900

    initial settings

---
+++ src/main/java/com/Buddymate/pickMate/PickMateApplication.java
@@ -0,0 +1,13 @@
+package com.Buddymate.pickMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Buddymate/pickMate/PickMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Buddymate.pickMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
