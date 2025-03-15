bca134d049563a9cbaaf392160831ad1792e0675
pangminwhere <qoendqh@naver.com>
2025-03-15 21:35:44 +0900

    fix: .env 파일 ë및 EnvConfig  수정

--- src/main/java/com/Buddymate/pickMate/config/EnvConfig.java
+++ src/main/java/com/Buddymate/pickMate/config/EnvConfig.java
@@ -3,12 +3,30 @@ package com.Buddymate.pickMate.config;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.StandardEnvironment;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class EnvConfig {
 
     @Bean
-    public Dotenv dotenv() {
-        return Dotenv.configure().ignoreIfMissing().load();
+    public StandardEnvironment environment() {
+        StandardEnvironment environment = new StandardEnvironment();
+
+        // `.env` 파일 로드
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        Map<String, Object> envVars = new HashMap<>();
+
+        dotenv.entries().forEach(entry -> envVars.put(entry.getKey(), entry.getValue()));
+
+        // 환경 변수 등록
+        MutablePropertySources propertySources = environment.getPropertySources();
+        propertySources.addFirst(new MapPropertySource("dotenv", envVars));
+
+        return environment;
     }
 }
