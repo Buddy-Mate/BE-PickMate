daf16a9614e478ac339235356ba3da1042a6e66d
pangminwhere <qoendqh@naver.com>
2025-05-12 18:27:11 +0900

    chore : ProjectRepositroy, StudyRepository JPA 쿼리 추가
    <commit message body>
    - 마감기한을 기준으로 expiredStatus가 false인 데이터 조회
    (findByDeadlineBeforeAndExpiredStatusFalse)

--- src/main/java/com/Buddymate/pickMate/repository/StudyRepository.java
+++ src/main/java/com/Buddymate/pickMate/repository/StudyRepository.java
@@ -1,13 +1,17 @@
 package com.Buddymate.pickMate.repository;
 
+import com.Buddymate.pickMate.entity.Project;
 import com.Buddymate.pickMate.entity.Study;
 import com.Buddymate.pickMate.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StudyRepository extends JpaRepository<Study, Long> {
     List<Study> findByAuthor(User author);
 
     List<Study> findByTitleContainingIgnoreCase(String keyword);
+
+    List<Study> findByDeadlineBeforeAndExpiredStatusFalse(LocalDateTime deadline);
 }
